afca460cc8ad64b3889cf540054afa235f35049a
Brad Boudreau <adigitalmonk@live.com>
2020-11-18 21:38:44 -0500

    Renaming project and improving some of the examples in the README

--- README.md
+++ README.md
@@ -1,15 +1,28 @@
-# Hephaestus.js - Dependency Injection
+# provide.js - Dependency Injection
+
+![CircleCI](https://circleci.com/gh/adigitalmonk/dependency-injector.svg?style=shield)
 
 A simple DI container inspired by [require.js](https://requirejs.org/).
 
+## Installation
+
+This package isn't in [npm](https://npmjs.com).
+You can install it, instead, using git.
+
+```
+npm install git+https://github.com/adigitalmonk/dependency-injector.git#master
+```
+
+And then it's available as `provide.js`!
+
 ## Usage
 
-There are two main touch points for the container, `define` and `forge`.
+There are two main touch points for the container, `define` and `provide`.
 
 The library returns an function that will create the container and provide the two primary access functions.
 
 ```javascript
-const { define, forge } = require('./src');
+const { define, provide } = require("provide.js");
 ```
 
 ### Defining dependencies
@@ -18,20 +31,20 @@ The purpose of `define` is to register a new "thing" into the container.  You ca
 ```javascript
 define("thing", () => "thing");
 define("pi", 3.1415);
-define("config", { "key": "alphanum"});
+define("config", { "key": "alphanum" });
 ```
 
-When defining a function, you can also provide a list of dependencies that will be injected into the function when it's called.
+When defining a function, you can also provide a list of dependencies that will be injected into the function when it"s called.
 
 ```javascript
 define("one", () => 1);
-define("useOne", ["one"], (one) => one + 2);
+define("useOne", ["one"], one => one + 2);
 ```
 
 Whatever is returned by your defined callback is what is injected into the function that uses it.
 The defined values are lazy-evaluated at the point of call instead of when registered.
 
-There are four arguments for `defined`:
+There are four arguments for `define`:
 - Name of the thing we're defining
 - Dependencies that the thing will ask for
 - The thing we're registering / defining
@@ -42,28 +55,28 @@ The registration process will adjust accordingly.
 
 Be warned, there is currently no logic to prevent circular dependencies.
 
-### Forging functions
+### Providing functions
 
-The purpose of `forge` is the execute some logic and return the value immediately.
+The purpose of `provide` is the execute some logic and return the value immediately.
 
 ```javascript
 define("version", { major: 1, minor: 3, patch: 4 });
 define("formatVersion", ["version"], (version) => `${version.major}.${version.minor}.${version.patch}`);
 
-forge(["formatVersion"], formatVersion => console.log(formatVersion)); // -> 1.3.4
+provide(["formatVersion"], formatVersion => console.log(formatVersion)); // -> 1.3.4
 ```
 
-While `define` is responsible for saving dependencies and what they resolve to, the `forge` function is designed to get the information back out.
+While `define` is responsible for saving dependencies and what they resolve to, the `provide` function is designed to get the information back out.
 
-The `forge` function only has two arguments and both are required.
+The `provide` function only has two arguments and both are required.
 
 - The dependencies to inject into a callback
 - A callback to inject the dependencies into and then execute
 
-The value returned by the callback is, in turn, returned by the `forge` function.
+The value returned by the callback is, in turn, returned by the `provide` function.
 
 ```javascript
-const version = forge(["version"], version => version);
+const version = provide(["version"], version => version);
 console.log(version); // -> { major: 1, minor: 3, patch: 4 }
 ```
 
@@ -73,9 +86,9 @@ We also have an exposed function that allows you to clone a container.
 This enables you to have different containers with some shared dependencies.
 
 ```javascript
-const hephaestus = require("hephaestus");
+const provide = require("provide.js");
 
-const constantContainer = hephaestus();
+const constantContainer = provide();
 constantContainer.define("pi", 3.1415);
 
 const formulaContainer = constantContainer.clone();
@@ -85,54 +98,59 @@ formulaContainer.define("surfaceAreaPizza", ["pi"], pi => radius => (pi * r) ** 
 // formulaContainer has both "pi" and "surfaceAreaPizza"
 ```
 
-
 ## Examples
 
 ### Express Middleware
 
 ```javascript
-// src/services/hephaestus.js
-const { define, forge } = require('hephaestus');
+// src/services/provider.js
+const { define, provide } = require("provide.js");
 define("logger", () => (params) => {
     console.log("Request Params:", params);
 });
-module.exports = forge;
+module.exports = provide;
 
 // src/middleware/logger.js
-module.exports = forge => 
-    forge(["logger"], (logger) => (req, _res, next) => {
-        logger(req.params);
-        next();
-    });
+module.exports = logger => (req, _res, next) => {
+    logger(req.params);
+    next();
+};
 
 // src/index.js
-const app = require('express')();
-const forge = require('./services/hephaestus')
-const loggerWare = require('./middleware/logger')(forge)
+const app = require("express")();
+const provide = require("./services/provider")
+const loggerWare = provide(["logger"], require("./middleware/logger"))
 
 app.use(loggerWare)
-// ...
+// etc
 ```
 
 ### Express Controller
 
 ```javascript
 // src/services/elasticSearch.js
-const { Client } = require('@elastic/elasticsearch')
-module.exports = new Client({ node: 'http://localhost:9200' });
+const { Client } = require("@elastic/elasticsearch")
+module.exports = elasticConfig => () => new Client({ node: "http://localhost:9200" })
 
-// src/services/hephaestus.js
-const { define, forge } = require('hephaestus');
-define("elasticSearch", require('./elasticSearch'));
-module.exports = forge;
+// src/services/provider.js
+const { define, provide } = require("provide.js");
+define("elasticConfig", () => ({ node: "http://localhost:9200" }))
+define("elasticSearch", ["elasticConfig"], require("./elasticSearch"));
+
+module.exports = provide;
+
+// src/controllers/health
+module.exports = elasticSearch => (req, res) => {
+    res.send(elasticSearch.cluster.health())
+}
 
 // src/index.js
-const app = require('express')();
-const forge = require('./services/hephaestus')
+const app = require("express")();
+const provide = require("./services/provider")
+const healthResource = require("./controllers/health");
 
-app.get('/health', forge(['elasticSearch'], (elasticSearch) => (req, res) => {
-    res.send(elasticSearch.cluster.health())
-}))
+app.get("/health", provide(["elasticSearch"], healthResource);
+// etc
 ```
 
 # Roadmap
--- test/test.js
+++ test/test.js
@@ -1,57 +1,57 @@
 const assert = require("assert");
-const hephaestus = require("../src");
+const providejs = require("../src");
 
 describe("Defining", () => {
     it("Returns true if successful", () => {
-        const { define } = hephaestus();
+        const { define } = providejs();
         assert(define("one", [], () => 1) === true);
     });
 
     it("Allows you to skip dependencies", () => {
-        const { define, forge } = hephaestus();
+        const { define, provide } = providejs();
         define("one", () => 1);
-        forge(["one"], (one) => {
+        provide(["one"], (one) => {
             assert(one === 1);
         });
     });
 
     it("Returns false if name in use", () => {
-        const { define } = hephaestus();
+        const { define } = providejs();
         define("one", () => 1);
         assert(define("one", () => 2) === false);
     });
 
     it("Returns allows override of name in use", () => {
-        const { define } = hephaestus();
+        const { define } = providejs();
         define("one", () => 1);
         assert(define("one", () => 2, { override: true }) === true);
     });
 
     it("Dependencies are lazy evaluated", () => {
-        const { define, forge } = hephaestus();
+        const { define, provide } = providejs();
         let value = 1;
         define("thing", () => value);
         value = 2;
-        assert(forge(["thing"], (thing) => thing) === value);
+        assert(provide(["thing"], (thing) => thing) === value);
     });
 
     // it("Doesn't allow circular dependencies", () => {
-    //     const { define, forge } = hephaestus();
+    //     const { define, provide } = providejs();
     //     define("one", ["two"], (two) => 1 + two);
     //     define("two", ["one"], (one) => one + 2);
-    //     forge(["one"], (one) => one); // exception?
+    //     provide(["one"], (one) => one); // exception?
     // });
 });
 
 describe("Forging", () => {
     it("Loads immediate children", () => {
-        const { define, forge } = hephaestus();
+        const { define, provide } = providejs();
 
         define("one", () => 1);
         define("two", () => 2);
         define("three", () => 3);
 
-        forge(["one", "two", "three"], (one, two, three) => {
+        provide(["one", "two", "three"], (one, two, three) => {
             assert(one === 1);
             assert(two === 2);
             assert(three === 3);
@@ -59,21 +59,21 @@ describe("Forging", () => {
     });
 
     it("Loads sub-dependencies", () => {
-        const { define, forge } = hephaestus();
+        const { define, provide } = providejs();
 
         define("one", () => 1);
         define("one-one", ["one"], (one) => one + 4);
-        forge(["one-one"], (one_one) => assert(one_one === 5));
+        provide(["one-one"], (one_one) => assert(one_one === 5));
     });
 
     it("Injects non-function dependencies", () => {
-        const { define, forge } = hephaestus();
+        const { define, provide } = providejs();
 
         define("one", 1);
-        assert(forge(["one"], (one) => one) === 1);
+        assert(provide(["one"], (one) => one) === 1);
 
         define("config", { "one": 1, "two": 2 });
-        assert(forge(["config"], (config) => {
+        assert(provide(["config"], (config) => {
             assert(config.one === 1);
             assert(config.two === 2);
 
@@ -82,66 +82,75 @@ describe("Forging", () => {
     });
 
     it("Loads sub-sub-dependencies", () => {
-        const { define, forge } = hephaestus();
+        const { define, provide } = providejs();
 
         define("one", () => 1);
         define("one-one", ["one"], (one) => one + 4);
         define("one-two", ["one-one"], (one_one) => one_one + 5);
-        forge(["one-two"], (one_two) => assert(one_two === 10));
+        provide(["one-two"], (one_two) => assert(one_two === 10));
     });
 
-    it("Returns the forgeback value", () => {
-        const { define, forge } = hephaestus();
+    it("Returns the defined value", () => {
+        const { define, provide } = providejs();
 
         define("one", () => 1);
-        assert(forge(["one"], one => one) === 1);
+        const result = provide(["one"], one => one);
+        assert(result === 1);
+    });
+
+    it("Provided callback's return value is returned", () => {
+        const { define, provide } = providejs();
+        define("callable", () => () => 1);
+
+        const callable = provide(["callable"], (callable) => callable);
+        assert(callable() === 1);
     });
 });
 
 describe("Subcontainers", () => {
     it("Clones itself", () => {
-        const containerA = hephaestus();
+        const containerA = providejs();
         containerA.define(["one"], () => 1);
         const containerB = containerA.clone();
 
-        assert(typeof(containerB) === "object");
-        assert(typeof(containerB.define) === "function");
-        assert(typeof(containerB.forge) === "function");
-        assert(typeof(containerB.clone) === "function");
+        assert(typeof (containerB) === "object");
+        assert(typeof (containerB.define) === "function");
+        assert(typeof (containerB.provide) === "function");
+        assert(typeof (containerB.clone) === "function");
     });
 
     it("Has working clones", () => {
-        const containerA = hephaestus();
+        const containerA = providejs();
         containerA.define("one", () => 1);
         const containerB = containerA.clone();
 
-        const resultA = containerA.forge(["one"], (one) => one);
-        const resultB = containerB.forge(["one"], (one) => one);
-        
+        const resultA = containerA.provide(["one"], (one) => one);
+        const resultB = containerB.provide(["one"], (one) => one);
+
         assert(resultA === resultB);
     });
 
     it("Clones have unique keys", () => {
-        const containerA = hephaestus();
+        const containerA = providejs();
         const containerB = containerA.clone();
         containerA.define("one", () => 1);
         containerB.define("two", () => 2);
 
-        assert(containerA.forge(["one"], (one) => one) === 1);
-        assert(containerB.forge(["one"], (one) => one) === undefined);
+        assert(containerA.provide(["one"], (one) => one) === 1);
+        assert(containerB.provide(["one"], (one) => one) === undefined);
 
-        assert(containerA.forge(["two"], (two) => two) === undefined);
-        assert(containerB.forge(["two"], (two) => two) === 2);
+        assert(containerA.provide(["two"], (two) => two) === undefined);
+        assert(containerB.provide(["two"], (two) => two) === 2);
     });
 
     it("Clones have unique entries per key", () => {
-        const containerA = hephaestus();
+        const containerA = providejs();
         const containerB = containerA.clone();
         assert(containerA.define("two", () => 2) === true);
         assert(containerB.define("two", () => "two") === true);
-        
-        const resA = containerA.forge(["two"], (two) => two);
-        const resB = containerB.forge(["two"], (two) => two);
+
+        const resA = containerA.provide(["two"], (two) => two);
+        const resB = containerB.provide(["two"], (two) => two);
         assert(resA !== resB);
     });
 });
